587b2292681d4b3304d3ae32f363bc35d818744f
rzkalee <kriz5638@gmail.com>
2025-10-30 20:00:48 +0700

    Tugas 3

---
+++ Pengenalan Dasar Python/Tugas/Tugas3.py
@@ -0,0 +1,15 @@
+suhu = 28
+status_ac = "Menyala" if suhu > 25 else "Mati"
+print(status_ac)
+#Output "Menyala"
+
+print(bin(12)) #0b1100
+print(bin(9)) #0b1001
+
+x = 12
+y = 9
+
+print( x & y ) 
+#Output = 8
+
+print(bin(8)) #1000
